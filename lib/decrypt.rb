9dd2095fb5f604153bfc9a9297387f06ef0cd7a8
Matthew Kimball <mekimba6@gmail.com>
2021-08-10 07:45:43 -0600

    removes pry

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -5,7 +5,6 @@ enigma = Enigma.new
 file = File.open("encrypted.txt", "r")
 file.each do |info|
   info.each_line do |line|
-    # require "pry"; binding.pry
     File.open("decrypted.txt", "w") { |f| f.write enigma.decrypt(line.chomp, @key, @date)}
     p "created 'decrypted.txt' with key #{@key} and date #{@date}"
   end
